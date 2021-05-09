949246fc6acea76e85ba9eaf0f52d910a3dda31d
6102bitcoin <sixoneohtwo@protonmail.com>
2021-05-09 21:00:53 +0100

    Add local node check

--- txCast_stagger.py
+++ txCast_stagger.py
@@ -2,30 +2,76 @@
 
 import time
 import requests
+import secrets
+import json
 from datetime import datetime
 from datetime import timedelta
 from stem import Signal
 from stem.control import Controller
-import secrets
 
-tor_password = "test" # Change this to your tor password
 tx_list = []
+failed_tx_list = []
 time_list = []
-next_broadcast_time = ""
-tx = ""
-ip = ""
-ip_tor = ""
 
-def get_ip():
-    global ip
-    ip = requests.get('http://httpbin.org/ip').text
-    ip = ip.partition('\"origin\": \"')[2]
-    ip = ip.rpartition('\"')[0] # Get only IP
+class RPCHost(object):
+    def __init__(self, url):
+        self._session = requests.Session()
+        self._url = url
+        self._headers = {'content-type': 'application/json'}
+    def call(self, rpcMethod, *params):
+        payload = json.dumps({"method": rpcMethod, "params": list(params), "jsonrpc": "2.0"})
+        tries = 5
+        hadConnectionFailures = False
+        while True:
+            try:
+                response = self._session.post(self._url, headers=self._headers, data=payload)
+            except requests.exceptions.ConnectionError:
+                tries -= 1
+                if tries == 0:
+                    raise Exception('Failed to connect for remote procedure call.')
+                hadFailedConnections = True
+                print("Couldn't connect for remote procedure call, will sleep for five seconds and then try again ({} more tries)".format(tries))
+                time.sleep(10)
+            else:
+                if hadConnectionFailures:
+                    print('Connected for remote procedure call after retry.')
+                break
+        if not response.status_code in (200, 500):
+            raise Exception('RPC connection failure: ' + str(response.status_code) + ' ' + response.reason)
+        responseJSON = response.json()
+        if 'error' in responseJSON and responseJSON['error'] != None:
+            raise Exception('Error in RPC call: ' + str(responseJSON['error']))
+        return responseJSON['result']
+
+def main():
+    global tor_password
+    tor_password = input("Enter tor password (set in .torrc): ")
+    print("--- Performing tor check ---")
+    renew_tor_ip()
+    ip_tor = get_ip_tor()
+    ip = get_ip()
     print("IP without tor: " + str(ip))
-    return ip
+    print("IP with tor: " + str(ip_tor))
+    if ip != ip_tor:
+        print("Success! tor is running")
+        set_endpoint()
+        set_node()
+        build_lists()
+        process_all()
+        print("")
+        print("############################# TXCAST COMPLETE #############################")
+    else:
+        print("")
+        print("Aborted, tor Not Connected")
+
+def renew_tor_ip():
+    with Controller.from_port(port=9051) as controller:
+        controller.authenticate(password=tor_password)
+        controller.signal(Signal.NEWNYM)
+    time.sleep(1) # Wait to ensure a new IP is in use
+    # What if next time is within 1 second?
 
 def get_ip_tor():
-    global ip_tor
     session = requests.session()
 
     # TO Request URL with SOCKS over TOR
@@ -40,38 +86,55 @@ def get_ip_tor():
     except Exception as e:
         print(str(e))
     else:
-        print("IP with tor: " + str(ip_tor))
         return ip_tor
 
-def renew_tor_ip():
-    with Controller.from_port(port=9051) as controller:
-        controller.authenticate(password=tor_password)
-        controller.signal(Signal.NEWNYM)
-    time.sleep(1) # Wait to ensure a new IP is in use
+def get_ip():
+    ip = requests.get('http://httpbin.org/ip').text
+    ip = ip.partition('\"origin\": \"')[2]
+    ip = ip.rpartition('\"')[0]
+    return ip
 
-def fuzz(exact_value, lower_limit_fraction, upper_limit_fraction):
-    lower_limit = int(exact_value * lower_limit_fraction)
-    upper_limit = int(exact_value * upper_limit_fraction)
-    fuzzed_value = exact_value + secrets.SystemRandom().randint(lower_limit, upper_limit)
-    return fuzzed_value
+def set_endpoint():
+    global endpoint
+    print("--- Service Selection ---")
+    service = input("Enter m for mempool.space / b for blockstream.info then press ENTER: ")
+    if service == "m" or "M":
+        endpoint = "http://mempoolhqx4isw62xs7abwphsq7ldayuidyx2v2oethdhhj6mlo2r6ad.onion/testnet/api/tx"
+    elif service == "b" or "B":
+        endpoint = "http://explorerzydxu5ecjrkwceayqybizmpjjznk5izmitf2modhcusuqlid.onion/testnet/api/tx"
+    else:
+        print("Input not recognized")
+        set_endpoint()
+    return    
 
-def get_tx_list():
-    global tx_next
-    global tx_list
-    finished = False
-    print("--- Enter Signed Transactions ---")
-    print("Paste signed transaction (CTRL-SHIFT-V) then press ENTER")
-    print("")
-    while not finished:
-        tx_next = input('tx: ')
-        if tx_next == "X" or tx_next == "x":
-            finished = True
-        else:
-            tx_list.append(tx_next)
-            print("")
-            print("Paste next transaction and press ENTER or Type X then press ENTER to END")
-    return
-
+def set_node():
+    global rpcPort
+    global rpcUser
+    global rpcPassword
+    global host
+    global ownNode
+    print("--- Own Node Config ---")
+    ownNode = input("Are you running bitcoind on this machine? Enter Y if yes, n press ENTER: ")
+    if ownNode == "Y" or "y":
+        ownNode = True
+        # Get node info
+        print("rpcPort defaults:")
+        print("     mainnet: 8332")
+        print("     testnet: 18332")
+        print("     signet:  38332")
+        print("     regtest: 18443")
+        rpcPort = input("Enter bitcoin rpcPort (e.g. 8332): ")
+        rpcUser = input("Enter bitcoin rpcUser: ")
+        rpcPassword = input("Enter bitcoin rpcPassword: ")
+        # Access RPC local server
+        serverURL = 'http://' + rpcUser + ':' + rpcPassword + '@localhost:' + str(rpcPort)
+        
+        # Using the class defined in the bitcoin_rpc_class.py
+        host = RPCHost(serverURL)
+    else:
+        ownNode = False
+        print("Download, install and configure bitcoin core.")
+    return  
 
 def build_lists():
     # Create randomly sorted list of transactions to broadcast:
@@ -108,32 +171,23 @@ def build_lists():
 
     return
 
-def push_tx(payload):
-    session = requests.session()
+def process_all():
+    for i in range(0, len(tx_list)):
+        print("")
+        result = process_tx(i)
 
-    # TO Request URL with SOCKS over TOR
-    session.proxies = {}
-    session.proxies['http']='socks5h://localhost:9050'
-    session.proxies['https']='socks5h://localhost:9050'
-
-    requests.post(endpoint, data=payload, proxies=session.proxies) 
-    print("Transaction Uploaded") 
-
-def set_endpoint():
-    global endpoint
-    print("--- Service Selection ---")
-    service = input("Enter m for mempool.space / b for blockstream.info then press ENTER: ")
-    if service == "m" or "M":
-        endpoint = "http://mempoolhqx4isw62xs7abwphsq7ldayuidyx2v2oethdhhj6mlo2r6ad.onion/testnet/api/tx"
-    elif service == "b" or "B":
-        endpoint = "http://explorerzydxu5ecjrkwceayqybizmpjjznk5izmitf2modhcusuqlid.onion/testnet/api/tx"
-    else:
-        set_endpoint()
-    return    
+        if ownNode:
+            if result[1] == False:
+                print("Transaction " + str(i+1) + "not detected in local mempool")
+                failed_tx_list.append(i)
+            else:
+                print("Transaction seen in local mempool")
+        else:
+            print("Transaction " + str(i+1) + "pushed to endpoint at " + str(result[0]))
+        
+        print(str(len(tx_list)-i-1) + " Transactions Remaining")
 
 def process_tx(i):
-    global next_broadcast_time
-
     renew_tor_ip()  # Renew tor IP address
 
     # Set broadcast values
@@ -151,30 +205,56 @@ def process_tx(i):
     push_tx(next_broadcast_tx)
     push_time = datetime.now()
 
-    return push_time
-
-
-def process_all():
-    for i in range(0, len(tx_list)):
-        print("")
-        print("Transaction " + str(i+1) + " broadcast at " + str(process_tx(i)))
-        print(str(len(tx_list)-i-1) + " Transactions Remaining")
-
-
-def main():
-    print("--- Performing tor check ---")
-    get_ip()
-    renew_tor_ip()  # Renew tor IP address
-    get_ip_tor()
-    if ip != ip_tor:
-        print("Success! tor is running")
-        set_endpoint()
+    if ownNode:
+        # Decode txid from raw transaction (using local node)
+        txid = host.call('decoderawtransaction', next_broadcast_tx)['txid']
+        
+        # Check if transaction has hit mempool
+        inMempool = False
+        attempts = 0
+        while not inMempool and attempts < 10:
+            inMempool = check_local_mempool(txid)
+            time.sleep(6)
+            attempts += 1
+            # What if next time is within 6 seconds?
     else:
-        print("Aborted, tor Not Connected")
-    build_lists()
-    process_all()
-    print("")
-    print("############################# TXCAST COMPLETE #############################")
+       inMempool = False
 
+    return push_time, inMempool
+
+def get_tx_list():
+    global tx_list
+    print("--- Enter Signed Transactions ---")
+    print("Paste signed transaction (CTRL-SHIFT-V) then press ENTER")
+    print("")
+    finished = False
+    while not finished:
+        tx_next = input('tx: ')
+        if tx_next == "X" or tx_next == "x":
+            finished = True
+        else:
+            tx_list.append(tx_next)
+            print("")
+            print("Paste next transaction and press ENTER or Type X then press ENTER to END")
+    return
+
+def push_tx(payload):
+    session = requests.session()
+
+    # TO Request URL with SOCKS over TOR
+    session.proxies = {}
+    session.proxies['http']='socks5h://localhost:9050'
+    session.proxies['https']='socks5h://localhost:9050'
+
+    requests.post(endpoint, data=payload, proxies=session.proxies) 
+    print("Transaction Uploaded") 
+
+def check_local_mempool(txid):
+    try:
+        host.call('getrawtransaction', txid)
+        inMempool = True
+    except:
+        inMempool = False
+    return inMempool
 
 main()
